64f78aebfef0634f1e430bd3e1f6b302ae0cae27
Francisco M. E <paco@vzenix.es>
2018-07-30 22:32:09 +0200

    Add sample configuration.

--- background.js
+++ background.js
@@ -16,6 +16,23 @@ function openViewer() {
     }
 }
 
+function loadSettings() {
+    var getting = browser.storage.local.get("color");
+    getting.then(onGot, onError);
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    function onGot(item) {
+        var color = "blue";
+        if (item.color) {
+            color = item.color;
+        }
+        document.body.style.border = "10px solid " + color;
+    }
+}
+
 if (typeof chrome !== typeof undefined && typeof chrome.browserAction !== typeof undefined) {
     chrome.browserAction.onClicked.addListener(openViewer);
 } else if (typeof browser !== typeof undefined && typeof browser.browserAction !== typeof undefined) {
