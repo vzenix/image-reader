f38e6011e324f97a5980e2925813d607ce6815e1
Francisco M. E <paco@vzenix.es>
2018-07-31 08:21:54 +0200

    Add options to save
    minWidthShow minHeightShow orderDefault orderDefaultDirection

--- config/js/options.js
+++ config/js/options.js
@@ -1,21 +1,28 @@
+/* minWidthShow minHeightShow orderDefault orderDefaultDirection */
+
 function saveOptions(e) {
     e.preventDefault();
     browser.storage.local.set({
-        color: document.querySelector("#color").value
+        minWidthShow: document.querySelector("#minWidthShow").value,
+        minHeightShow: document.querySelector("#minHeightShow").value,
+        orderDefault: document.querySelector("#orderDefault").value,
+        orderDefaultDirection: document.querySelector("#orderDefaultDirection").value
     });
 }
 
 function restoreOptions() {
-
     function setCurrentChoice(result) {
-        document.querySelector("#color").value = result.color || "blue";
+        document.querySelector("#minWidthShow").value = result.minWidthShow || 32;
+        document.querySelector("#minHeightShow").value = result.minHeightShow || 32;
+        document.querySelector("#orderDefault").value = result.orderDefault || "A01";
+        document.querySelector("#orderDefaultDirection").value = result.orderDefaultDirection || "DESC";
     }
 
     function onError(error) {
         console.log(`Error: ${error}`);
     }
 
-    var getting = browser.storage.local.get("color");
+    var getting = browser.storage.local.get(["minWidthShow", "minHeightShow", "orderDefault", "orderDefaultDirection"]);
     getting.then(setCurrentChoice, onError);
 }
 
